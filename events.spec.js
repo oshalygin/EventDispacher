9008558e38f07f323a67d2c53157501aac89c745
oshalygin@gmail.com <oshalygin@gmail.com>
2016-02-18 23:21:40 -0800

    added another test case to quickly test another event name

--- events.spec.js
+++ events.spec.js
@@ -1,13 +1,13 @@
 var Events = require('./events');
 
-describe("Event Dispatcher", function() {
-    it("is an object", function() {
+describe("Event Dispatcher", function () {
+    it("is an object", function () {
 
         expect(typeof Events).toBe("object");
     });
 
-    it("can register a callback", function() {
-        var actual = Events.on('foo', function() {
+    it("can register a callback", function () {
+        var actual = Events.on('foo', function () {
             return 'bar';
         })();
 
@@ -15,16 +15,16 @@ describe("Event Dispatcher", function() {
         expect(actual).toBe(expected);
     });
 
-    it("can register a callback with a scope", function() {
-        Events.on('foo', function() {
+    it("can register a callback with a scope", function () {
+        Events.on('foo', function () {
             return 'bar';
         }, this);
     });
 
-    it("can trigger an event", function() {
+    it("can trigger an event", function () {
         var bar = 1;
 
-        Events.on('foo', function() {
+        Events.on('foo', function () {
             bar = 2;
         });
 
@@ -32,10 +32,10 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(2);
     });
 
-    it("can trigger an event with arguments", function() {
+    it("can trigger an event with arguments", function () {
         var bar = 1;
 
-        Events.on('foo', function(v) {
+        Events.on('foo', function (v) {
             bar = v;
         });
 
@@ -44,12 +44,12 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(5);
     });
 
-    it("can trigger multiple callbacks on an event", function() {
+    it("can trigger multiple callbacks on an event", function () {
 
-        (function() {
+        (function () {
             var baz = 'HauteLook';
 
-            Events.on('foo', function() {
+            Events.on('foo', function () {
                 expect(baz).toBe('Nordstomrack');
             });
 
@@ -57,7 +57,7 @@ describe("Event Dispatcher", function() {
         })();
 
         var bar = 1;
-        Events.on('foo', function() {
+        Events.on('foo', function () {
             bar = 2;
         });
 
@@ -65,10 +65,10 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(2);
     });
 
-    it("can remove callbacks from an event", function() {
+    it("can remove callbacks from an event", function () {
         var bar = 1;
 
-        Events.on('foo', function() {
+        Events.on('foo', function () {
             bar += 1;
         });
 
@@ -78,14 +78,14 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(2);
     });
 
-    it("can remove specific callbacks from an event", function() {
+    it("can remove specific callbacks from an event", function () {
         var bar = 1;
 
-        var adder = function(v) {
+        var adder = function (v) {
             bar += v;
         };
 
-        var multiplier = function(v) {
+        var multiplier = function (v) {
             bar *= v;
         };
 
@@ -96,5 +96,28 @@ describe("Event Dispatcher", function() {
         Events.off('foo', adder);
         Events.trigger('foo', 100);
         expect(bar).toBe(200);
+    });
+
+    it("event names are independent of each other", () => {
+        var foo = 1;
+
+        Events.on("foo", () => {
+            foo = 3;
+        });
+
+        Events.trigger("foo");
+        expect(foo).toBe(3);
+
+        Events.on("baz", () => {
+            foo = 4;
+        });
+
+        Events.trigger("baz");
+        expect(foo).toBe(4);
+
+        Events.off("foo");
+        Events.trigger("foo");
+        expect(foo).toBe(4);
+
     });
 });
