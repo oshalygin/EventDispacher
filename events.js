475f1a5e106c210ec2fe1ed4cd25e468481c923f
oshalygin@gmail.com <oshalygin@gmail.com>
2016-02-18 23:03:47 -0800

    rough working copy

--- events.js
+++ events.js
@@ -1,3 +1,38 @@
-module.exports = (function() {
-    return {};
+
+
+module.exports = (() => {
+    "use strict";
+    let eventName;
+
+    let eventWithCallbacks = {
+        eventName: eventName,
+        callbacks: new Map()
+    };
+
+    function setEventCallback(eventName, callback, scope) {
+        eventWithCallbacks.eventName = eventName;
+        eventWithCallbacks.callbacks.set(callback.toString(), callback)
+
+        return callback;
+    }
+    function removeEventCallback(eventName, callback) {
+        if (callback === undefined) {
+            eventWithCallbacks.callbacks.clear();
+            return;
+        }
+        eventWithCallbacks.callbacks.delete(callback.toString());
+    }
+
+    function triggerEvent(eventName, parameters) {
+        for (let callback of eventWithCallbacks.callbacks.values()) {
+            callback.call(this, parameters);
+        }
+    }
+
+    return {
+        on: setEventCallback,
+        off: removeEventCallback,
+        trigger: triggerEvent
+    };
+
 })();
