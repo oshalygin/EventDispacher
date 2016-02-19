7bef23407eb386fe20cb8e1973b20a8a096a9b17
oshalygin@gmail.com <oshalygin@gmail.com>
2016-02-18 23:22:30 -0800

    renamed to events

--- events.js
+++ events.js
@@ -1,38 +1,38 @@
 module.exports = (() => {
     "use strict";
 
-    let eventWithCallbacks = new Map();
+    let events = new Map();
 
     function setEventCallback(eventName, callback, scope) {
 
-        if (eventWithCallbacks.has(eventName)) {
-            let callbacks = eventWithCallbacks.get(eventName);
+        if (events.has(eventName)) {
+            let callbacks = events.get(eventName);
             callbacks.set(callback.toString(), callback);
-            eventWithCallbacks.set(eventName, callbacks);
+            events.set(eventName, callbacks);
         }
         else {
             let callbacks = new Map();
             callbacks.set(callback.toString(), callback);
-            eventWithCallbacks.set(eventName, callbacks);
+            events.set(eventName, callbacks);
         }
 
         return callback;
     }
     function removeEventCallback(eventName, callback) {
 
-        let callbacks = eventWithCallbacks.get(eventName);
+        let callbacks = events.get(eventName);
         if (callback === undefined) {
             callbacks.clear();
-            eventWithCallbacks.set(eventName, callbacks);
+            events.set(eventName, callbacks);
             return;
         }
 
         callbacks.delete(callback.toString());
-        eventWithCallbacks.set(eventName, callbacks);
+        events.set(eventName, callbacks);
     }
 
     function triggerEvent(eventName, parameters) {
-        let callbacks = eventWithCallbacks.get(eventName);
+        let callbacks = events.get(eventName);
         for (let callback of callbacks.values()) {
             callback.call(this, parameters);
         }
