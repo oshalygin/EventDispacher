d01a3b01696860a81070b4fc06cfffd4480f1154
oshalygin@gmail.com <oshalygin@gmail.com>
2016-02-18 22:38:54 -0800

    flipped every test on and added an expectation to the first test case

--- events.spec.js
+++ events.spec.js
@@ -6,19 +6,22 @@ describe("Event Dispatcher", function() {
         expect(typeof Events).toBe("object");
     });
 
-    xit("can register a callback", function() {
-        Events.on('foo', function() {
+    it("can register a callback", function() {
+        var actual = Events.on('foo', function() {
             return 'bar';
-        });
+        })();
+
+        var expected = "bar";
+        expect(actual).toBe(expected);
     });
 
-    xit("can register a callback with a scope", function() {
+    it("can register a callback with a scope", function() {
         Events.on('foo', function() {
             return 'bar';
         }, this);
     });
 
-    xit("can trigger an event", function() {
+    it("can trigger an event", function() {
         var bar = 1;
 
         Events.on('foo', function() {
@@ -26,11 +29,10 @@ describe("Event Dispatcher", function() {
         });
 
         Events.trigger('foo');
-
         expect(bar).toBe(2);
     });
 
-    xit("can trigger an event with arguments", function() {
+    it("can trigger an event with arguments", function() {
         var bar = 1;
 
         Events.on('foo', function(v) {
@@ -42,11 +44,11 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(5);
     });
 
-    xit("can trigger multiple callbacks on an event", function() {
+    it("can trigger multiple callbacks on an event", function() {
 
         (function() {
             var baz = 'HauteLook';
-            
+
             Events.on('foo', function() {
                 expect(baz).toBe('Nordstomrack');
             });
@@ -63,7 +65,7 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(2);
     });
 
-    xit("can remove callbacks from an event", function() {
+    it("can remove callbacks from an event", function() {
         var bar = 1;
 
         Events.on('foo', function() {
@@ -76,7 +78,7 @@ describe("Event Dispatcher", function() {
         expect(bar).toBe(2);
     });
 
-    xit("can remove specific callbacks from an event", function() {
+    it("can remove specific callbacks from an event", function() {
         var bar = 1;
 
         var adder = function(v) {
@@ -90,8 +92,7 @@ describe("Event Dispatcher", function() {
         Events.on('foo', adder);
         Events.trigger('foo', 1);
         expect(bar).toBe(2);
-
-        Events.on('foo', multiplier)
+        Events.on('foo', multiplier);
         Events.off('foo', adder);
         Events.trigger('foo', 100);
         expect(bar).toBe(200);
